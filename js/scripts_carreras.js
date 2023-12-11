4d8f2f04c05d0c00275917b9d29e82c205ebafc8
2325Matias <126356735+2325Matias@users.noreply.github.com>
2023-12-11 17:05:47 -0300

    v2

--- js/scripts_carreras.js
+++ js/scripts_carreras.js
@@ -1,18 +1,3 @@
-document.getElementById('MateriasId').addEventListener('change', function () {
-    var selectedValue = this.value;
-
-    // Ocultar todos los grupos de checkboxes
-    document.querySelectorAll('.checkbox-group').forEach(function (group) {
-        group.style.display = 'none';
-    });
-
-    // Mostrar el grupo de checkboxes correspondiente a la opción seleccionada
-    var selectedGroup = document.getElementById('checkboxes-' + selectedValue);
-    if (selectedGroup) {
-        selectedGroup.style.display = 'block';
-    }
-});
-
 function toggleCheckboxes(checkbox) {
     // Obtener todos los checkboxes excepto el de "Ninguno"
     var checkboxes = document.querySelectorAll('[name^="ninguno"], [name^="INFD"], [name^="Bicentenario"], [name^="Pueblos"], [name^="Progresar"], [name^="AUH"], [name^="docente"], [name^="Envion"], [name^="Argentina"], [name^="Ellas"]');
